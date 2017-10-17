771e3a79e51d4578acbf3df17d04a22d8f997eb2
IgnacioYanjari <ignacio.yanjari@mail.udp.cl>
2017-10-17 00:10:43 -0300

    init

---
+++ Web/app/config.py
@@ -0,0 +1,5 @@
+host = "localhost"
+port = 4243
+user = "tics"
+password = "1234"
+database = "tics"
---
+++ Web/app/views.py
@@ -0,0 +1,20 @@
+from app import app
+from datetime import datetime
+from flask import render_template,request,redirect
+from config import *
+import psycopg2
+import time
+import serial
+
+conn = psycopg2.connect("dbname=%s host=%s user=%s password=%s"%(database,host,user,password))
+cur = conn.cursor()
+
+@app.route('/')
+@app.route('/index')
+def index():
+    arduinoPort = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    time.sleep(2)
+    getSerialValue=arduinoPort.readline()
+    print("Valor : ", getSerialValue)
+    return render_template("index.html",temperatura=getSerialValue)
+    #return render_template("index.html")

d0e408190c4cffa54c4a0913b55125a486746b4e
IgnacioYanjari <ignacio.yanjari@mail.udp.cl>
2017-10-22 20:35:31 -0300

    Update Implementados.ino

--- Codigos Arduino/Sensores_implementados.ino
+++ Codigos Arduino/Sensores_implementados.ino
@@ -12,7 +12,7 @@ float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 #define Offset 0.00            //deviation compensate
 #define LED 13
 #define samplingIntervalph 20
-#define printIntervalph 800
+#define printIntervalph 500
 #define ArrayLenth  40    //times of collection
 int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
 int pHArrayIndex=0;
@@ -27,7 +27,7 @@ int pHArrayIndex=0;
 const byte numReadings = 20;     //the number of sample times
 byte ECsensorPin = A1;  //EC Meter analog output,pin on analog 1
 byte DS18B20_Pin = 2; //DS18B20 signal, pin on digital 2
-unsigned int AnalogSampleInterval=25,printIntervalec=700,tempSampleInterval=850;  //analog sample interval;serial print interval;temperature sample interval
+unsigned int AnalogSampleInterval=25,printIntervalec=500,tempSampleInterval=650;  //analog sample interval;serial print interval;temperature sample interval
 unsigned int readings[numReadings];      // the readings from the analog input
 byte index = 0;                  // the index of the current reading
 unsigned long AnalogValueTotal = 0;                  // the running total
@@ -46,7 +46,7 @@ void setup() {
 
   //Sensor Electroconductividad
 
-  // initialize all the readings to 0:
+ // initialize all the readings to 0:
   for (byte thisReading = 0; thisReading < numReadings; thisReading++)
     readings[thisReading] = 0;
   TempProcess(StartConvert);   //let the DS18B20 start the convert
@@ -63,8 +63,8 @@ void loop() {
   logR2 = log(R2);
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
   Tc = T - 273.15;
-  Serial.println(Tc);
-  delay(500);
+  
+  
 
   //Código Sensor Ph
 
@@ -79,19 +79,14 @@ void loop() {
       pHValue = 3.5*voltage+Offset;
       samplingTime=millis();
   }
-  if(millis() - printTime > printIntervalph)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
-  {
-    Serial.print("    pH value: ");
-    Serial.println(pHValue,2);
-    digitalWrite(LED,digitalRead(LED)^1);
-    printTime=millis();
-  }
+  
 
   //Código Sensor Electroconductividad
-/*
    /*
    Every once in a while,sample the analog value and calculate the average.
-  if(millis()-AnalogSampleTime>=AnalogSampleInterval)  
+  */
+
+  if(millis()-AnalogSampleTime>=AnalogSampleInterval)
   {
     AnalogSampleTime=millis();
      // subtract the last reading:
@@ -112,42 +107,55 @@ void loop() {
   /*
    Every once in a while,MCU read the temperature from the DS18B20 and then let the DS18B20 start the convert.
    Attention:The interval between start the convert and read the temperature should be greater than 750 millisecond,or the temperature is not accurate!
-   if(millis()-tempSampleTime>=tempSampleInterval) 
+  */
+   if(millis()-tempSampleTime>=tempSampleInterval)
   {
     tempSampleTime=millis();
     temperature = TempProcess(ReadTemperature);  // read the current temperature from the  DS18B20
     TempProcess(StartConvert);                   //after the reading,start the convert for next reading
+
   }
-   /*
-   Every once in a while,print the information on the serial monitor.
-  if(millis()-printTime>=printIntervalec)
+  if(millis() - printTime > printIntervalph)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
   {
+    Serial.print("    pH value: ");
+    Serial.print(pHValue,2);
+    digitalWrite(LED,digitalRead(LED)^1);
     printTime=millis();
     averageVoltage=AnalogAverage*(float)5000/1024;
-    Serial.print("Analog value:");
-    Serial.print(AnalogAverage);   //analog average,from 0 to 1023
-    Serial.print("    Voltage:");
-    Serial.print(averageVoltage);  //millivolt average,from 0mv to 4995mV
-    Serial.print("mV    ");
-    Serial.print("temp:");
-    Serial.print(temperature);    //current temperature
-    Serial.print("^C     EC:");
-    
+    //Serial.print("Analog value:");
+    //Serial.print(AnalogAverage);   //analog average,from 0 to 1023
+    //Serial.print("    Voltage:");
+    //Serial.print(averageVoltage);  //millivolt average,from 0mv to 4995mV
+    //Serial.print("mV    ");
+    Serial.print("  Temperatura: ");
+    Serial.println(Tc);
+    //Serial.print("temp:");
+    //Serial.print(temperature);    //current temperature
+    //Serial.print("^C     EC:");
+
     float TempCoefficient=1.0+0.0185*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.0185*(fTP-25.0));
-    float CoefficientVolatge=(float)averageVoltage/TempCoefficient;   
-    if(CoefficientVolatge<150)Serial.println("No solution!");   //25^C 1413us/cm<-->about 216mv  if the voltage(compensate)<150,that is <1ms/cm,out of the range
-    else if(CoefficientVolatge>3300)Serial.println("Out of the range!");  //>20ms/cm,out of the range
-    else
-    { 
+    float CoefficientVolatge=(float)averageVoltage/TempCoefficient;
+    //if(CoefficientVolatge<150)Serial.println("No solution!");   //25^C 1413us/cm<-->about 216mv  if the voltage(compensate)<150,that is <1ms/cm,out of the range
+    //else if(CoefficientVolatge>3300)Serial.println("Out of the range!");  //>20ms/cm,out of the range
+    //else
+    //{
       if(CoefficientVolatge<=448)ECcurrent=6.84*CoefficientVolatge-64.32;   //1ms/cm<EC<=3ms/cm
       else if(CoefficientVolatge<=1457)ECcurrent=6.98*CoefficientVolatge-127;  //3ms/cm<EC<=10ms/cm
       else ECcurrent=5.3*CoefficientVolatge+2278;                           //10ms/cm<EC<20ms/cm
       ECcurrent/=1000;    //convert us/cm to ms/cm
+      Serial.print(" EC : ");
+      if(ECcurrent >= 0 ){
       Serial.print(ECcurrent,2);  //two decimal
       Serial.println("ms/cm");
-    }
+      }
+      else{
+        Serial.println("No Solution ");
+      }
+      
+    //}
+    
   }
-  */
+  delay(500);
 }
 
 

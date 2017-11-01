810d34708514ca7baabd26c6211ec624ba951a55
IgnacioYanjari <nach116@hotmail.com>
2017-11-01 20:19:17 -0300

    Comparador.ino
    Agregando Comparador de sensor creado :)

---
+++ Codigos Arduino/Comparador.ino
@@ -0,0 +1,128 @@
+//Sensor Usado Para Comparar
+#include <OneWire.h>
+OneWire  ds(10);  // on pin 10
+
+
+//Sensor Creado
+int ThermistorPin = 0;
+int Vo;
+float R1 = 10000;
+float logR2, R2, T; 
+float Tc, Tf;
+//float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+  //Sensor Usado Para Comparar 
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+  
+  if ( !ds.search(addr)) {
+    //Serial.println("No more addresses.");
+    //Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  
+  //Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    //Serial.write(' ');
+    //Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+     // Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      //Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      //Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      //Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      //Serial.println("Device is not a DS18x20 family device.");
+      return;
+  } 
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1);         // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  //Serial.print("  Data = ");
+  //Serial.print(present,HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    //Serial.print(data[i], HEX);
+    //Serial.print(" ");
+  }
+  //Serial.print(" CRC=");
+  //Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+
+  // convert the data to actual temperature
+
+  unsigned int raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // count remain gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    if (cfg == 0x00) raw = raw << 3;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw << 2; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw << 1; // 11 bit res, 375 ms
+    // default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("Sensor Temperatura Original : ");
+  Serial.print("  Temperature : ");
+  Serial.print(celsius);
+  Serial.print(" Celsius, ");
+
+
+  // Sensor Creado.
+  Vo = analogRead(ThermistorPin);
+  //R2 = R1 * (1023.0 / (float)Vo - 1.0);
+  //logR2 = log(R2);
+  //T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  //Tc = T - 273.15;
+  //Tf = (Tc * 9.0)/ 5.0 + 32.0; 
+  Serial.print("Sensor Temperatura Creado");
+  Serial.print("V : ");
+  Serial.print(Vo);
+  //Serial.print("Temperature: "); 
+  //Serial.print(Tc);
+  //Serial.println(" C");   
+  delay(500);
+}
+

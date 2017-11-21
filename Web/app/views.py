a6c1b3a8374bada3d60b7fae63c000d25ca12674
IgnacioYanjari <ignacio.yanjari@mail.udp.cl>
2017-11-21 00:02:23 -0300

    SQL para displays

--- Web/app/views.py
+++ Web/app/views.py
@@ -1,16 +1,29 @@
 from app import app
 from datetime import datetime , time , date
 from flask import render_template,request,redirect
+import psycopg2
 
 from config import *
-import psycopg2
 conn = psycopg2.connect("dbname=%s host=%s user=%s password=%s"%(database,host,user,password))
 cur = conn.cursor()
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index',methods=['POST'])
 def index():
-    return render_template("index.html")
+    sql = """ SELECT tipo_agua FROM tipos_aceptados GROUP BY tipo_agua;"""
+    cur.execute(sql)
+    tipo_agua = cur.fetchall()
+    #print(tipo_agua)
+
+    sql="""SELECT nombre_tipo FROM tipos_aceptados """
+    cur.execute(sql)
+    nombres_aceptados = cur.fetchall()
+
+    if request.method == 'POST':
+        return render_template("index.html")
+
+
+    return render_template("index.html",tipo_agua2 = tipo_agua ,nombres_aceptados = nombres_aceptados)
 
 @app.route('/data',methods=['GET','POST'])
 def data():
